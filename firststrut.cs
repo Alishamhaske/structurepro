a34e87ee66a8ac649fe740efc250d62c4ef0a9f3
Alishamhaske <mmhaske2001@gmail.com>
2023-09-20 16:56:01 +0530

    Add project files.

---
+++ firststrut.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopfirstclass_program.structurepro
+{
+    public class firststrut
+    {
+        static void Main(string[] args)
+        {
+            screen s1 = new screen(11, 12);
+            Console.WriteLine(s1.print());
+        }
+    }
+    //
+
+    public struct screen
+    {
+        int x, y;
+        public screen(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public string print()
+        {
+            return $"x is: {x} , y is: {y}";
+
+        }
+    }
+}
